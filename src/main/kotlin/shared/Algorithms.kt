092c3cb3de1094b0b8ffcd995058908f147b0cbd
wkt <waikon@waikontse.info>
2022-12-25 21:04:34 +0100

    feat(day 12): Add day 12 solution

--- src/main/kotlin/shared/Algorithms.kt
+++ src/main/kotlin/shared/Algorithms.kt
@@ -1,4 +1,65 @@
 package shared
 
+import java.util.*
+
 class Algorithms {
+    data class Edge(val endPoint: Node, val cost: Int)
+    data class GraphSize(val width: Int, val height: Int)
+
+    class Graph(val size: GraphSize) {
+        private val graph: MutableMap<Node, MutableList<Edge>> = mutableMapOf()
+
+        init {
+            (0 until (size.height*size.width)).onEach { graph[it] = mutableListOf() }
+        }
+
+        fun addEdges(node: Node, edges:List<Edge>) {
+            graph[node]?.addAll(edges)
+        }
+
+        fun getEdges(node: Node): List<Edge> {
+            return graph[node]?.toList() ?: listOf()
+        }
+
+        fun vertices(): Set<Node>
+                = graph.keys
+
+        fun verticesCount(): Int
+                = graph.keys.size
+
+        fun print()
+                = graph.keys.onEach { println("From: $it with edges: ${getEdges(it)}") }
+    }
+
+    companion object {
+         fun dijkstra(firstNode: Node, graph: Graph): Pair<IntArray, Map<Node, Edge>> {
+            val distances = IntArray(graph.verticesCount()) { if (it == firstNode) 0 else Int.MAX_VALUE }
+            val visited = BooleanArray(graph.verticesCount()) { it == firstNode }
+            val pathMap = mutableMapOf<Node, Edge>()
+            val pq = PriorityQueue<Edge> { left, right -> left.cost - right.cost }
+
+             pq.offer(Edge(firstNode, 0))
+
+            while(pq.isNotEmpty()) {
+                val u = pq.poll().endPoint
+                val distU = distances[u]
+
+                for (edge in graph.getEdges(u)) {
+                    val distV = distances[edge.endPoint]
+                    val pathWeight = edge.cost + distU
+
+                    if (!visited[edge.endPoint] || (distV > pathWeight)) {
+                        visited[edge.endPoint] = true
+                        distances[edge.endPoint] = pathWeight
+                        pathMap[edge.endPoint] = edge
+                        pq.offer(Edge(edge.endPoint, pathWeight))
+                    }
+                }
+            }
+
+            return Pair(distances, pathMap)
+        }
+    }
 }
+
+typealias Node = Int
---
+++ src/main/kotlin/week2/HillClimbing.kt
@@ -0,0 +1,127 @@
+package week2
+
+import shared.Algorithms.*
+import shared.Algorithms.Companion.dijkstra
+import shared.Node
+import shared.Puzzle
+
+class HillClimbing: Puzzle(12) {
+    override fun solveFirstPart(): Any {
+        val input = puzzleInput
+        val cleanedInput = input.map { it.replace("S", "a") }
+            .map { it.replace("E", "z") }
+
+        val graph = parseMap(cleanedInput)
+
+        val shortestPaths = dijkstra(findStartingPosition(input), graph)
+
+        return shortestPaths.first[findEndingPosition(input)]
+    }
+
+    override fun solveSecondPart(): Any {
+        val input = puzzleInput
+        val cleanedInput = input.map { it.replace("S", "a") }
+            .map { it.replace("E", "z") }
+
+        val graph = parseMap(cleanedInput)
+
+
+        return cleanedInput.joinToString("")
+            .mapIndexed { index, c -> if (c == 'a') index else -1 }
+            .filterNot { i -> i == -1 }
+            .map { dijkstra(it, graph) }
+            .map { it.first[findEndingPosition(input)] }
+            .min()
+
+        //val shortestPaths = dijkstra(findStartingPosition(input), graph)
+
+        //return shortestPaths.first[findEndingPosition(input)]
+    }
+
+    private fun parseMap(map: List<String>): Graph {
+        val width = map[0].length
+        val height = map.size
+        val size = GraphSize(width, height)
+        val graph = Graph(size)
+
+        return parseGraph2(map.joinToString(separator = ""), size, 0, graph)
+    }
+
+    private tailrec fun parseGraph2(
+        map: String,
+        size: GraphSize,
+        currPos: Node,
+        graph: Graph): Graph {
+
+        if (currPos >= size.width * size.height) {
+            return graph
+        }
+
+        val edges = getConnectedEdges(map, size, currPos)
+        graph.addEdges(currPos, edges)
+
+        return parseGraph2(map, size, currPos.inc(), graph)
+    }
+
+    private fun getConnectedEdges(map: String, size: GraphSize, currPos: Node): List<Edge> {
+        val edges = mutableListOf<Edge>()
+
+        if (isConnectedTop(map, size, currPos)) {
+            edges.add(Edge(currPos.minus(size.width),1))
+        }
+        if (isConnectedBottom(map, size, currPos)) {
+            edges.add(Edge(currPos.plus(size.width),1))
+        }
+        if (isConnectedLeft(map, size, currPos)) {
+            edges.add(Edge(currPos.dec(),1))
+        }
+        if (isConnectedRight(map, size, currPos)) {
+            edges.add(Edge(currPos.inc(),1))
+        }
+
+        return edges
+    }
+
+    private fun isConnectedTop(map: String, size: GraphSize, pos: Int): Boolean {
+        return isConnected(map, size, pos, pos.minus(size.width))
+    }
+
+    private fun isConnectedBottom(map: String, size: GraphSize, pos: Int): Boolean {
+        return isConnected(map, size, pos, pos.plus(size.width))
+    }
+
+    private fun isConnectedLeft(map: String, size: GraphSize, pos: Int): Boolean {
+        return isConnected(map, size, pos, pos.dec())
+    }
+
+    private fun isConnectedRight(map: String, size: GraphSize, pos: Int): Boolean {
+        return isConnected(map, size, pos, pos.inc())
+    }
+
+    private fun isConnected(map: String, size: GraphSize, from: Int, to: Int): Boolean {
+        if (to < 0) {
+            return false
+        } else if (to >= size.width * size.height) {
+            return false
+        }
+
+        return (map[from].code - map[to].code) >= -1
+    }
+
+    private fun findStartingPosition(map: List<String>): Int {
+        return findPositionOfChar(map.joinToString(separator = ""), 'S', 0)
+    }
+
+    private fun findEndingPosition(map: List<String>): Int {
+        return findPositionOfChar(map.joinToString(separator = ""), 'E', 0)
+    }
+
+    private tailrec fun findPositionOfChar(map: String, target: Char, currPos: Int): Int {
+        if (map[currPos] == target) {
+            return currPos
+        }
+
+        return findPositionOfChar(map, target, currPos.inc())
+    }
+}
+
