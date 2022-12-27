ed5be44d7905e37c6435a2428b41450b48706d38
Wai kon Tse <waikon@waikontse.info>
2022-12-27 23:32:47 +0100

    feat: Add day 14 and 25 skeleton solution

---
+++ src/main/kotlin/week3/HotAirBalloons.kt
@@ -0,0 +1,21 @@
+package week3
+
+import shared.Puzzle
+
+class HotAirBalloons : Puzzle(25) {
+    override fun solveFirstPart(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun solveSecondPart(): Any {
+        TODO("Not yet implemented")
+    }
+
+    private fun toSnafu(decimal: String) {
+
+    }
+
+    private fun toDecimal(snafu: String) {
+
+    }
+}
